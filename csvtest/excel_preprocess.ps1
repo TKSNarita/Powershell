6b27064bacfa8ef7a10e07600cfc1185a39e07ef
TKSNarita <tks.narita@gmail.com>
2020-09-29 20:51:48 +0900

    ログファイルの出力

--- csvtest/excel_preprocess.ps1
+++ csvtest/excel_preprocess.ps1
@@ -1,3 +1,18 @@
+
+# 日付を変数に格納
+$logTime = (Get-Date).ToString("yyyy-MM-dd-hh-mm")
+
+# ログファイル格納場所を変数に格納
+$logpath = "C:\Users\tksna\OneDrive\デスクトップ\csvtest\log"
+
+# ログファイル名を格納
+$logname = $logpath + "\" + "log$logTime.log"
+
+# ログ出力開始宣言
+Start-Transcript $logname
+
+echo "$logtime：ExcelのCSV変換を開始します"
+
 # Excelを操作宣言
 $excel = New-Object -ComObject Excel.Application
 
@@ -6,6 +21,13 @@ $excel.Visible = $False
 
 # 対象ファイルを変数に格納
 $tmp = "C:\Users\tksna\OneDrive\デスクトップ\csvtest\test.xlsx"
+
+# 対象ファイルを別名で保存する
+# $tmp2 = "C:\Users\tksna\OneDrive\デスクトップ\cvtest2\test.xlsx"
+# ファイルのコピーと、読み取り専用属性を無効に設定
+# Copy-Item -Path $tmp -Destination $tmp2;
+# Set-ItemProperty -Path $tmp2 -Name IsReadOnly -Value $false;
+
 
 # 変換（コンバート）後のファイル名を変数に格納
 $savePath = "C:\Users\tksna\OneDrive\デスクトップ\csvtest\finaltest.csv"
@@ -18,7 +40,8 @@ $savePath = "C:\Users\tksna\OneDrive\デスクトップ\csvtest\finaltest.csv"
 Remove-Item $savePath 
 
 # Excelファイルを開く
-$book = $excel.Workbooks.Open($tmp)
+# https://docs.microsoft.com/ja-jp/office/vba/api/excel.workbooks.open
+$book = $excel.Workbooks.Open($tmp, [Type]::Missing, [Type]::Missing, [Type]::Missing, [Type]::Missing, [Type]::Missing,$True)
 
 # ワークシートを番号で指定し接続
 $sheet = $excel.Worksheets.Item(1)
@@ -60,6 +83,13 @@ $book.SaveAs($savepath,6)
 # Excelを閉じる
 $excel.Quit()
 
+echo "Excelを閉じました"
+
 # プロセスを解放する
 $excel = $null
+
+echo "Excelのプロセス解放完了"
+
+Stop-Transcript
+
 [GC]::Collect()
