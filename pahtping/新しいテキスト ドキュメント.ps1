6d6c7ab943712d3df32367a09dbadbba0d1b7eee
TKSNarita <tks.narita@gmail.com>
2020-10-14 06:20:16 +0900

    verup

--- pahtping/新しいテキスト ドキュメント.ps1
+++ pahtping/新しいテキスト ドキュメント.ps1
@@ -4,28 +4,20 @@ $row = (Get-Content .\nettest.csv).Length
 #s—ñ‚ð•Ï”‚É‘ã“ü
 $line = Import-Csv .\nettest.csv
 
-for ($i=0; $i -lt $row; $i++){
+for ($j=1; $j -lt 4; $j++){
 
-$date = Get-Date -Format "yyyy-MMdd-HHmmss"
-$n1 = $line[$i+1].block + "block_1‰ñ–Ú"
+    for ($i=0; $i -lt $row; $i++){
 
-echo $n1 >> .\trace.log
-echo $date >> .\trace.log
-pathping -n $line[$i+1].ip >>trace.log
+    $date = Get-Date -Format "yyyy-MMdd-HHmmss"
+    $n1 = $line[$i].block + "_block_" + $j + "‰ñ–Ú"
 
-$date = Get-Date -Format "yyyy-MMdd-HHmmss"
-$n2 = $line[$i+1].block + "block_2‰ñ–Ú"
+    echo $n1 | Tee-Object -FilePath trace.log
+    echo $date | Tee-Object -FilePath trace.log
+    pathping -n $line[$i].ip | Tee-Object -FilePath trace.log
 
-echo $n2 >> .\trace.log
-echo $date >> .\trace.log
-pathping -n $line[$i+1].ip >>trace.log
+    echo "---------------------------------------------------------------------------------------" | Tee-Object -FilePath trace.log
 
-$date = Get-Date -Format "yyyy-MMdd-HHmmss"
-$n3 = $line[$i+1].block + "block_3‰ñ–Ú"
-
-echo $n3 >> .\trace.log
-echo $date >> .\trace.log
-pathping -n $line[$i+1].ip >>trace.log
+    }
 
 }
 
